5aa010b137e02e7d46768822a00c795faec5c9ae
naabilazakiyah <naabilazakiyah@gmail.com>
2026-05-18 23:38:27 +0700

    footer admin

---
+++ admin_footer.php
@@ -0,0 +1,9 @@
+
+<footer class="footer">
+  <div class="container footer-inner">
+    <div class="footer-brand">HN Amanah Travel</div>
+  </div>
+</footer>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
